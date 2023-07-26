51bf53e75d886e55cb4140584a607d0331729713
Rekxis <Rekxis@gmx.de>
2023-07-26 09:24:22 +0200

    create object of Konto and manipulate it, with output

--- com/cc/java/App.java
+++ com/cc/java/App.java
@@ -1,9 +1,20 @@
 package com.cc.java;
 
+
 public class App {
     
     public static void main(String[] args) {
-    
+        Konto konto = new Konto(100);
+        int kontostand = konto.getKontostand();
+        String kontoAlsText = Integer.toString(kontostand);
+
+        output(kontoAlsText);
+
+        konto.setKontostand(5000);
+        kontostand = konto.getKontostand();
+        kontoAlsText = Integer.toString(kontostand);
+        
+        output(kontoAlsText);
     }
 
 
