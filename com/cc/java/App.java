4e1a24c5bfb45cffbeb471185a06d554caca4447
Rekxis <Rekxis@gmx.de>
2023-07-26 12:18:34 +0200

    console output via output method + rewrote output

--- com/cc/java/App.java
+++ com/cc/java/App.java
@@ -1,32 +1,34 @@
 package com.cc.java;
 
-
 public class App {
-    
+
     public static void main(String[] args) {
         Konto konto1 = new Konto();
         Konto konto2 = new Konto();
         Konto konto3 = new Konto();
 
-        System.out.println(konto1.getKontostand());
-        System.out.println(konto2.getKontostand());
-        System.out.println(konto3.getKontostand());
-        
+        output(konto1.getKontostand());
+        output(konto2.getKontostand());
+        output(konto3.getKontostand());
+
         System.out.println("------------------------");
 
         konto1.setKontostand(konto1.getKontostand() * 2);
         konto2.setKontostand(konto2.getKontostand() * 3);
         konto3.setKontostand(konto3.getKontostand() * 10);
 
-        System.out.println(konto1.getKontostand());
-        System.out.println(konto2.getKontostand());
-        System.out.println(konto3.getKontostand());
+        output(konto1.getKontostand());
+        output(konto2.getKontostand());
+        output(konto3.getKontostand());
 
     }
 
+    // private static void output(String outputStr) {
+    //     System.out.println(outputStr);
+    // }
 
-    private static void output(String outputStr) {
-        System.out.println(outputStr);
-    } 
+    private static void output(int outputInt) {
+        System.out.println(outputInt);
+    }
 
 }
